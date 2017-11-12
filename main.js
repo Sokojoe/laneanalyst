1bd6b4718a73ca2126d260723a3c06fde6ce2585
Sokjoe <Sokojoey@gmail.com>
2017-11-12 02:08:01 -0500

    Add .gitignore file, mongodb packages. Rm .env file

--- main.js
+++ main.js
@@ -8,4 +8,20 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname + '/view/main.html'));
 });
 
+function updatedb () {
+var MongoClient = require('mongodb').MongoClient, 
+	 assert = require('assert');
+
+  // Connection URL
+  var url = process.env.MONGODB_URI;
+
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+      console.log("Connected successfully to server");
+        db.close();
+	});
+}
+
+updatedb()
 app.listen(process.env.PORT || 5000)
