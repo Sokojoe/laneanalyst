0ba1084604568d35b7178fd60015d27c67c3fc53
Sokjoe <Sokojoey@gmail.com>
2018-01-10 00:32:37 -0500

    Restructured view, added server response to xhttp request

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-app.use(express.static(__dirname + '/view'));
+app.use(express.static(__dirname + '/view/js'));
+app.use(express.static(__dirname + '/view/css'));
+app.use(express.static(__dirname + '/view/img'));
 
 app.get('/', function(req, response) {
   if (req.query.c1 != undefined && req.query.c2 != undefined){
-    console.log(req.query.c1, req.query.c2)
-  }
-  response.sendFile(path.join(__dirname + '/view/main.html'));
-  
+    console.log("Received request for:", req.query.c1, req.query.c2)
+    response.send("Recieved request for champions: " + req.query.c1 + ", " + req.query.c2)
+  } else {
+  response.sendFile(path.join(__dirname + '/view/index.html'));
+  } 
 });
 
 function connectToDB () {
@@ -17,15 +20,14 @@ var MongoClient = require('mongodb').MongoClient,
 	 assert = require('assert');
 
   // Connection URL
-  var url = process.env.MONGODB_URI;
+  var url = process.env.MONGODB_URI
   
-  console.log(url)
   // Use connect method to connect to the server
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
       console.log("Connected successfully to server");
-      db.close();
       });
 }
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000)
+connectToDB()
--- view/js/main.js
+++ view/js/main.js
@@ -1,11 +1,20 @@
 function getChamps(){
   var c1 = document.getElementById("champ1").value
   var c2 = document.getElementById("champ2").value
-  var xhttp = new XMLHttpRequest();
-  var url = "http://localhost:5000/?c1=" +c1 +"&c2=" + c2 
+  var url = "?c1=" +c1 +"&c2=" + c2 
   xhttp.open("GET", url, true)
   xhttp.send()
 }
 
 function setdata(){
+
+} 
+
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function (){
+  if (xhttp.readyState == 4){ 
+  console.log(xhttp.responseText)
+  }
 }
