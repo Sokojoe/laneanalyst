91cc6453921a32b7adf4642fc355c2025aa60f56
Sokjoe <Sokojoey@gmail.com>
2018-01-20 13:09:58 -0500

    upgraded to express handlebars

--- main.js
+++ main.js
@@ -2,23 +2,24 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fs = require( 'fs')
-const handlebars = require('handlebars')
+const exphbs = require('express-handlebars')
 
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(path.join(__dirname, '/static')))
 
-app.get('/', function(req, response) {
-  response.sendFile(path.join(__dirname + '/view/index.html'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', function(req, res) {
+  res.render('search')
 })
 
 app.get('/compare', function (req, res) {
-  fs.readFile(path.join(__dirname + '/view/compare.html'), 'utf-8', function(err, src) {
-    var c1 = req.query.c1
-    var c2 = req.query.c2
-    connectToDB(c1, c2, res, src)
-  })
+  var c1 = req.query.c1
+  var c2 = req.query.c2
+  connectToDB(c1, c2, res)
 })
 
-function connectToDB (c1, c2, res, src) {
+function connectToDB (c1, c2, res) {
   var MongoClient = require('mongodb').MongoClient
   // Connection URL
   var url = process.env.MONGODB_URI
@@ -29,9 +30,7 @@ function connectToDB (c1, c2, res, src) {
         c1: doc["data"][c1],
         c2: doc["data"][c2]
       }
-      var template = handlebars.compile(src);
-      var html = template(data);
-      res.send(html)
+      res.render('compare', data)
     })
     client.close()
   })
